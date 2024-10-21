462a5e90e85c31c70bf1b9ecb9c192e89adf39c9
Wayde-Ouadie <ouadie.xyz1@gmail.com>
2024-10-21 16:03:07 +0100

    Change printf to ft_fprintf in pwd

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: oel-feng <oel-feng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 10:43:00 by oel-feng          #+#    #+#             */
-/*   Updated: 2024/10/21 15:59:07 by oel-feng         ###   ########.fr       */
+/*   Updated: 2024/10/21 16:00:16 by oel-feng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ bool	my_pwd(t_commands *cmnds, t_shell *shell, int flag)
 		if (!cwd)
 			return (free(cwd), ft_putendl_fd("Error getting pwd", STDERR_FILENO), shell->exit_status = EXIT_FAILURE, true);
 	}
-	ft_printf(STDOUT_FILENO, "%s\n", buff);
+	ft_fprintf(STDOUT_FILENO, "%s\n", buff);
 	free(cwd);
 	shell->exit_status = EXIT_SUCCESS;
 	return (true);
